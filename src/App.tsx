f7066ca49fa0c49a54947f9f932c1a2d89228f32
Ricardo Santos da Silva <ricardosantos@MacBook-Air-de-Ricardo.local>
2022-11-15 21:36:48 -0300

    realizando testes

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+
 import './App.css';
 import CurrencyInput from './components/CurrencyInput';
-import { TextField, InputAdornment } from '@material-ui/core';
 
 
 function App() {
   const [numero,setNumero] = useState(0)
 
-  
   return (
     <div className={'App'}>
       <CurrencyInput 
-      variant='outlined'
-      numero={numero} color="primary" autoFocus label='valor' onChange={setNumero} />
+        variant='outlined'
+        numero={numero} 
+        color="primary" 
+        autoFocus 
+        label='valor' 
+        onChange={setNumero} 
+      />
 
     </div>
   );
